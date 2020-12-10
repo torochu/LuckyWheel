65fc0143fdd96846d1b8d2da56f87b0f57c9f78b
ToroChu <whileonrails@gmail.com>
2020-12-11 00:47:12 +0800

    i dunno

--- js/event.js
+++ js/event.js
@@ -1,17 +1,25 @@
-import {fetchEvent, fetchEventMembers, fetchEventGifts} from './api.js'
+import { fetchEvent, fetchEventMembers, fetchEventGifts } from './api.js';
 // import timer from './timer.js'
 
-const colors = ['#f1e8e6', '#bbd4ce', '#64379f', '#81e9e6', '#2c698d', '#2a6fdb', '#ffcb00','#543c52', '#fefcbf', '#ff6150', '#e3f6f5', '#072448', '#f9b4ab', '#fdebd3', '#bbd4ce', '#309975', '#ddacf5', '#dad873', '#bae8e8', '#f55951', '#edd2cb', '#95adbe']
+const colors = ['#ffd15b', '#ffe99c', '#ffbe5b'];
 
+const rotateBtn = document.getElementById('rotateBtn');
+const pie = document.getElementById('pie');
+const giftsWrapper = document.querySelector('#giftsWrapper');
+const textsWrapper = document.querySelector('#textsWrapper');
 
-let event = {}
-let eventGifts = []
-let eventMembers = []
+const heading = document.querySelector('.heading');
 
+let itemAngle = 0; // 每個扇型佔有多少角度
+let startAngle = 0; // 起始角度
+let goToAngle = 0; // 終點角度
+let actualDegree; // 中獎角度
+let event = {};
+let eventGifts = [];
+let eventMembers = [];
 
 let timer = (now, countDownDate) => {
-  let x = setInterval(function() {
-
+  let x = setInterval(function () {
     // Get today's date and time
     let now = new Date().getTime();
 
@@ -20,101 +28,124 @@ let timer = (now, countDownDate) => {
 
     // Time calculations for days, hours, minutes and seconds
     let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
     let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     // Display the result in the element with id="demo"
-    document.getElementById("app").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
+    document.getElementById('app').innerHTML =
+      days + 'D：' + hours + 'H：' + minutes + 'M：' + seconds + 'S ';
 
     // If the count down is finished, write some text
     if (distance < 0) {
       clearInterval(x);
-      document.getElementById("demo").innerHTML = "EXPIRED";
+      document.getElementById('demo').innerHTML = 'EXPIRED';
     }
   }, 1000);
-}
+};
+
+// 打亂獎項 order
+let shuffle = function (a, b) {
+  let num = Math.random() > 0.5 ? -1 : 1;
+  return num;
+};
 
 let renderWheel = () => {
-  console.log('render wheel')
-  const pie = document.querySelector('#pie')
-  const pieText = document.querySelector('#pieText')
-  let itemAngle = 360 / eventGifts.length; // 每個扇型佔有多少角度
-  let startAngle = 15;  // 指針起始角度
-  let textAngle = 0;
-  let skewedAngle = -90 + itemAngle;
-  let rotateAngle = 0; // 旋轉從 0 開始
-  let pieTemplate = ''
-  let textTemplate = ''
+  console.log('渲染轉盤 開始');
+  // const oddEven =
+  //   eventGifts.length % 2 === 0 ? console.log('雙') : console.log('單');
+  let gifts = eventGifts.sort(shuffle); // 打亂排序的獎項 array
+  // console.log('判斷雙單數', oddEven);
+  itemAngle = 360 / eventGifts.length;
 
-  console.log('rotate angle ', rotateAngle)
-  console.log('skewedAngle ', skewedAngle)
-  console.log('item angle: ', itemAngle)
-  
+  let skewedAngle = -90 + itemAngle;
+  console.log('skewedAngle: ', skewedAngle);
+  let rotateAngle = 0; // 旋轉從 0 開始
+  let pieTemplate = '';
+  let textTemplate = '';
 
   eventGifts.forEach((item, index) => {
     let textTemp = `
-      <li class="slice textSlice">
-        <p style="transform: rotate(0deg);">
-          ${ index }
-        </p>
-      </li>
-    `
+      <div class="textBox" style="transform: rotate(${
+        rotateAngle + itemAngle / 2
+      }deg);">
+          ${item.giftName}
+      </div>
+    `;
+    console.log('index num: ', index % 3);
+    let pieTemp = `
+      <div 
+        data-gift="${index}"
+        class="slice" 
+        style="
+          transform: rotate(${rotateAngle}deg) skewY(${skewedAngle}deg); 
+          background: ${colors[index % 3]}
+      "></div>
+    `;
+    console.log('字體反轉: ', 90 - itemAngle);
     textTemplate += textTemp;
+    pieTemplate += pieTemp;
 
-    // let pieTemp = `
-    //   <li class="slice" style="transform: rotate(${ rotateAngle }deg) skewY(${ skewedAngle }deg); background-color: ${colors[index]};">
-    //     <div style="">
-    //       <p style="
-    //         transform: rotate(0deg) skewY(${90-itemAngle}deg);
-    //       ">
-          
-    //       </p>
-    //     </div>
-    //   </li>
-    // `
-      console.log('字體反轉: ', 90-itemAngle)
-    // console.log(temp)
-    // pieTemplate += pieTemp;
     rotateAngle -= itemAngle; // 旋轉 - angle
-  })
-  pieText.innerHTML = textTemplate
-  pie.innerHTML = pieTemplate
-  console.log('pieText: ', pieText)
-  console.log('pieTemplate: ', pieTemplate)
-}
+  });
+
+  textsWrapper.innerHTML = textTemplate;
+  giftsWrapper.innerHTML = pieTemplate;
+
+  console.log(textsWrapper);
+};
+
+let spin = (e) => {
+  console.log('開始旋轉');
+  let rndDrawIndex = Math.round(Math.random() * eventGifts.length);
+  console.log('抽出得獎項： ', eventGifts[rndDrawIndex]);
+
+  goToAngle = startAngle + (rndDrawIndex * itemAngle + 360 * 4);
+  pie.style.transition = 'all 4s ease-out';
+  pie.style.transform = `rotate(${goToAngle}deg)`;
+  console.log('goAngle', goToAngle);
+};
+
+let afterTransition = (e) => {
+  console.log('e: ', e);
+  actualDegree = goToAngle % 360; // 取得餘數, 真實的degree 中獎角度
+  console.log('actual degree: ', actualDegree);
+  // pie.style.transform = `rotate(${actualDegree}deg)`
+};
 
 async function getData(eventID) {
   try {
-    await fetchEventGifts(eventID).then(res => eventGifts = res.data)
-    await fetchEventMembers(eventID).then(res => eventMembers = res.data)
-    renderWheel()
+    await fetchEventGifts(eventID).then((res) => (eventGifts = res.data));
+    await fetchEventMembers(eventID).then((res) => (eventMembers = res.data));
+    renderWheel();
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
 }
 
-(function () {
-  const eventID = window.location.search.split('=')[1]
+rotateBtn.addEventListener('click', spin);
+pie.addEventListener('transitionend', afterTransition);
 
-  fetchEvent(eventID)
-  .then(res => {
+(function () {
+  const eventID = window.location.search.split('=')[1];
+
+  fetchEvent(eventID).then((res) => {
     if (res.status === 200) {
-      event = {...res.data}
-      console.log('fetchEvent', res)
-      let now = new Date().getTime()
+      event = { ...res.data };
+      heading.textContent = event.name;
+      console.log('fetchEvent', res);
+      let now = new Date().getTime();
       if (now > Date.parse(event.start)) {
-        console.log('活動開始')
-        getData(eventID)
-      } else if (now > Date.parse(event.end)){
-        console.log('活動已經結束')
-        
+        console.log('活動開始');
+        getData(eventID);
+      } else if (now > Date.parse(event.end)) {
+        console.log('活動已經結束');
       } else {
-        console.log('活動倒數')
-        timer(now, Date.parse(event.start))
-        
+        console.log('活動倒數');
+        timer(now, Date.parse(event.start));
       }
     }
-  })
+  });
 })();
--- js/event_list.js
+++ js/event_list.js
@@ -1,40 +1,43 @@
-import apiService from './apiService.js'
+import apiService from './apiService.js';
 
 const eventList = document.getElementById('event-list');
 let events = [];
 
 (function () {
-  return apiService.fetchEvents()
-    .then( (res) => {
+  return apiService
+    .fetchEvents()
+    .then((res) => {
       if (res.status === 200) {
         events = res.data;
-        console.log(res)
-        render()
+        console.log(res);
+        render();
       } else {
-        eventList.innerText = `錯誤： ${ res.status }`
+        eventList.innerText = `錯誤： ${res.status}`;
       }
     })
-    .catch( (err) => {
-      console.log(err)
-      eventList.innerText = "錯誤"
-    })
+    .catch((err) => {
+      console.log(err);
+      eventList.innerText = '錯誤';
+    });
 })();
 
 let activeEvents = (currentDate, items) => {
-  return items.filter( item => {
-    return Date.parse(item.start) < currentDate && Date.parse(item.end) > currentDate
-  })
-}
+  return items.filter((item) => {
+    return (
+      Date.parse(item.start) < currentDate && Date.parse(item.end) > currentDate
+    );
+  });
+};
 
 let render = () => {
-  let lists = ''
-  const currentDate = Date.parse(new Date())
+  let lists = '';
+  const currentDate = Date.parse(new Date());
   // const activeEventsArr = activeEvents(currentDate, events)
-  
+
   // activeEventsArr.forEach((item) => {
-    events.forEach((item) => {
-    let str = `<li><a href="./event.html?id=${ item.id }">${ item.name }</a></li>`
-    lists += str
-  })
-  eventList.innerHTML = lists
-}
+  events.forEach((item) => {
+    let str = `<li><a href="./event.html?id=${item.id}">${item.name}</a></li>`;
+    lists += str;
+  });
+  eventList.innerHTML = lists;
+};
