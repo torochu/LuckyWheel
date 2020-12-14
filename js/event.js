ea9a780dabbffc62a064af216ec71258b4e2f312
ToroChu <whileonrails@gmail.com>
2020-12-14 11:04:22 +0800

    fixed half & single full slice

--- js/event.js
+++ js/event.js
@@ -6,21 +6,21 @@ import {
   removeEventMember,
   createEventHistory,
   fetchEventHistory,
-} from "./api.js";
+} from './api.js';
 // import timer from './timer.js'
 
-const colors2 = ["#ffd15b", "#ffe99c"];
-const colors = ["#ffd15b", "#ffe99c", "#ffbe5b"];
+const colors2 = ['#ffd15b', '#ffe99c'];
+const colors = ['#ffd15b', '#ffe99c', '#ffbe5b'];
 
-const pie = document.getElementById("pie");
-const giftsWrapper = document.querySelector("#giftsWrapper");
-const textsWrapper = document.querySelector("#textsWrapper");
-const prizeWon = document.querySelector("#prizeWon");
-const player = document.getElementById("player");
-const heading = document.querySelector(".heading");
-const rotateBtn = document.getElementById("rotateBtn");
-const nextPlayer = document.getElementById("nextPlayer");
-const winnerList = document.getElementById("winner_list");
+const pie = document.getElementById('pie');
+const giftsWrapper = document.querySelector('#giftsWrapper');
+const textsWrapper = document.querySelector('#textsWrapper');
+const prizeWon = document.querySelector('#prizeWon');
+const player = document.getElementById('player');
+const heading = document.querySelector('.heading');
+const rotateBtn = document.getElementById('rotateBtn');
+const nextPlayer = document.getElementById('nextPlayer');
+const winnerList = document.getElementById('winner_list');
 
 let itemAngle = 0; // 每個扇型佔有多少角度
 let startAngle = 0; // 起始角度
@@ -53,13 +53,13 @@ let timer = (now, countDownDate) => {
     let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     // Display the result in the element with id="demo"
-    document.getElementById("app").innerHTML =
-      days + "D：" + hours + "H：" + minutes + "M：" + seconds + "S ";
+    document.getElementById('app').innerHTML =
+      days + 'D：' + hours + 'H：' + minutes + 'M：' + seconds + 'S ';
 
     // If the count down is finished, write some text
     if (distance < 0) {
       clearInterval(x);
-      document.getElementById("demo").innerHTML = "EXPIRED";
+      document.getElementById('demo').innerHTML = 'EXPIRED';
     }
   }, 1000);
 };
@@ -97,7 +97,7 @@ let assemblePieSlice = (item, idx, rotateAngle, skewedAngle, oddEven) => {
 };
 
 let renderWheel = () => {
-  console.log("渲染轉盤 開始");
+  console.log('渲染轉盤 開始');
 
   // 隨機挑選玩家
   selector = eventMembers[Math.floor(Math.random() * eventMembers.length)];
@@ -110,15 +110,53 @@ let renderWheel = () => {
   let skewedAngle = -90 + itemAngle;
   // console.log("skewedAngle: ", skewedAngle);
   let rotateAngle = 0; // 旋轉從 0 開始
-  let pieTemplate = "";
-  let textTemplate = "";
+  let pieTemplate = '';
+  let textTemplate = '';
 
   switch (eventGifts.length) {
     case 1:
-      console.log("只剩一個禮物");
+      console.log('只剩一個禮物');
+      eventGifts.forEach((item, index) => {
+        textTemplate += `
+          <div
+            class="textBox"
+            style="
+              transform: rotate(${rotateAngle}deg);
+          ">
+            ${item.giftName}
+          </div>
+        `;
+
+        pieTemplate += `<div data-gift="${item.id}" class="slice-full"
+        style="background: ${oddEven[index % oddEven.length]}"
+        ></div>`;
+        rotateAngle -= itemAngle; // 旋轉 - angle
+      });
       break;
     case 2:
-      console.log("只剩 2 個禮物");
+      console.log('只剩 2 個禮物');
+      eventGifts.forEach((item, index) => {
+        textTemplate += `
+          <div
+            class="textBox"
+            style="
+              transform: rotate(${rotateAngle + itemAngle / 2}deg);
+          ">
+            ${item.giftName}
+          </div>
+        `;
+
+        pieTemplate += `
+          <div 
+            data-gift="${item.id}"
+            class="slice-half" 
+            style="
+              transform: rotate(${rotateAngle}deg) skewY(0deg); 
+              background: ${oddEven[index % oddEven.length]}
+            ">
+          </div>`;
+        rotateAngle -= itemAngle; // 旋轉 - angle
+      });
       break;
     // case 3:
     //   console.log("只剩 3 個禮物");
@@ -159,7 +197,7 @@ async function modifyList(obj) {
     // giftId: selectedGift.id,
     // memberId: selector.id,
   };
-  console.log("historyObj:", historyObj);
+  console.log('historyObj:', historyObj);
   // debugger;
   try {
     // debugger;
@@ -171,7 +209,7 @@ async function modifyList(obj) {
     // 新增得獎者資訊至 eventHistory
     await createEventHistory(historyObj);
   } catch (err) {
-    console.log("錯誤：", err);
+    console.log('錯誤：', err);
   }
 
   console.log(historyObj);
@@ -179,7 +217,7 @@ async function modifyList(obj) {
 
 // FUNCTION: 下一回合
 let nextRound = () => {
-  console.log("next round");
+  console.log('next round');
   const item = {
     event,
     gift: selectedGift,
@@ -209,7 +247,7 @@ let nextRound = () => {
 // FUNCTION: 轉盤開始旋轉
 let spin = (e) => {
   rotateBtn.disabled = true;
-  console.log("開始旋轉");
+  console.log('開始旋轉');
   let rndDrawIndex = Math.floor(Math.random() * eventGifts.length);
 
   // console.log("randomIndex: ", rndDrawIndex);
@@ -219,7 +257,7 @@ let spin = (e) => {
   // console.log("抽出得獎項： ", selectedGift);
 
   goToAngle = startAngle + (rndDrawIndex * itemAngle + 360 * 4);
-  pie.style.transition = "all 4s ease-out";
+  pie.style.transition = 'all 4s ease-out';
   pie.style.transform = `rotate(${goToAngle}deg)`;
   // console.log("goAngle", goToAngle);
 };
@@ -229,25 +267,34 @@ let afterTransition = (e) => {
   actualDegree = goToAngle % 360; // 取得餘數, 真實的degree 中獎角度
   // console.log("actual degree: ", actualDegree);
   // pie.style.transform = `rotate(${actualDegree}deg)`
-
-  // 把抽中的 slice 換顏色
   const selectedSlice = document.querySelector(
     `[data-gift="${selectedGift.id}"]`
   );
-  selectedSlice.classList.add("selected");
+
+  // 把抽中的 slice 換顏色
+  // switch (eventGifts.length) {
+  //   case 1:
+  //     pie.classList.add('selected');
+  //     break;
+  //   case 2:
+  //     break;
+  //   default:
+  selectedSlice.classList.add('selected');
+  //     break;
+  // }
 
   // 把獎項放到 DOM
   prizeWon.textContent = selectedGift.giftName;
 
   // 把 ＧＯ 按鈕置換成 下一位 按鈕
   setTimeout(() => {
-    rotateBtn.classList.add("d-none");
-    nextPlayer.classList.remove("d-none");
+    rotateBtn.classList.add('d-none');
+    nextPlayer.classList.remove('d-none');
   }, 2000);
 };
 
 let renderEventHistory = () => {
-  let historyTempalte = "";
+  let historyTempalte = '';
   for (let item of eventWinners) {
     console.log(item);
     historyTempalte += `<li>${item.memberName} 抽到了 ${item.giftName}</li>`;
@@ -260,9 +307,9 @@ async function getData(eventID) {
     await fetchEventGifts(eventID).then((res) => (eventGifts = res.data));
     await fetchEventMembers(eventID).then((res) => (eventMembers = res.data));
     await fetchEventHistory(eventID).then((res) => (eventWinners = res.data));
-    console.log("目前禮品數量：", eventGifts.length);
-    console.log("目前參與者人數: ", eventMembers.length);
-    console.log("得獎資料: ", eventWinners.length);
+    console.log('目前禮品數量：', eventGifts.length);
+    console.log('目前參與者人數: ', eventMembers.length);
+    console.log('得獎資料: ', eventWinners.length);
     renderWheel();
     renderEventHistory();
   } catch (err) {
@@ -270,12 +317,12 @@ async function getData(eventID) {
   }
 }
 
-rotateBtn.addEventListener("click", spin);
-pie.addEventListener("transitionend", afterTransition);
-nextPlayer.addEventListener("click", nextRound);
+rotateBtn.addEventListener('click', spin);
+pie.addEventListener('transitionend', afterTransition);
+nextPlayer.addEventListener('click', nextRound);
 
 (function () {
-  const eventID = window.location.search.split("=")[1];
+  const eventID = window.location.search.split('=')[1];
   // const obj = { id: 1234456677, name: "test" };
   // const postpost = createEventHistory(obj).then((res) => {
   // console.log("response", res);
@@ -289,12 +336,12 @@ nextPlayer.addEventListener("click", nextRound);
       // console.log("fetchEvent", res);
       let now = new Date().getTime();
       if (now > Date.parse(event.start)) {
-        console.log("活動開始");
+        console.log('活動開始');
         getData(eventID);
       } else if (now > Date.parse(event.end)) {
-        console.log("活動已經結束");
+        console.log('活動已經結束');
       } else {
-        console.log("活動倒數");
+        console.log('活動倒數');
         timer(now, Date.parse(event.start));
       }
     }
